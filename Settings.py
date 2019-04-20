2428f2dc19e8348b0739075dc658914a118a10df
Kevin Russell <kevinr.work@gmail.com>
2019-04-20 00:10:24 -0400

    Split GUI to Settings.py

--- Settings.py
+++ Settings.py
@@ -3,7 +3,12 @@ from tkinter import messagebox
 
 
 class SettingsGUI:
-    def __init__(self):
+    def __init__(self, header=None):
+        if header:
+            self.header_text = header
+        else:
+            self.header_text = 'Welcome to Send to LV Settings!\nSettings can be changed below.\nPress save when finished'
+
         self.main = Tk()
         self.server = StringVar()
         self.database = StringVar()
@@ -11,11 +16,15 @@ class SettingsGUI:
         self.email_port = StringVar()
         self.email_user_name = StringVar()
         self.email_user_pass = StringVar()
+        self.email_from = StringVar()
+        self.email_to = StringVar()
+        self.email_cc = StringVar()
 
     def build_gui(self):
         # Set GUI Geometry and GUI Title
-        self.main.geometry('500x565+500+300')
-        self.main.title('Send to LV Add Settings')
+        self.main.geometry('509x565+500+100')
+        self.main.title('Send to LV Settings')
+        self.main.resizable(False, False)
 
         # Set GUI Frames
         header_frame = Frame(self.main)
@@ -32,16 +41,15 @@ class SettingsGUI:
         header_frame.pack()
         network_frame.pack(fill="both")
         email_frame.pack(fill="both")
-        econn_frame.grid(row=3, column=0, ipady=14)
-        emsg_frame.grid(row=3, column=1)
+        econn_frame.grid(row=3, column=0, ipady=4)
+        emsg_frame.grid(row=3, column=1, ipady=20)
         stlv_frame.pack(fill="both")
         stlv_list_frame.pack(in_=stlv_frame, fill="both", side=RIGHT)
         stlv_settings_frame.pack(in_=stlv_frame, fill="both", side=LEFT)
         buttons_frame.pack()
 
         # Apply Header text to Header_Frame that describes purpose of GUI
-        header_text = 'Welcome to Send to LV! Settings haven''t been established.\nPlease fill out all empty fields below:'
-        header = Message(self.main, text=header_text, width=375, justify=CENTER)
+        header = Message(self.main, text=self.header_text, width=375, justify=CENTER)
         header.pack(in_=header_frame)
 
         # Apply Network Labels & Input boxes to the Network_Frame
@@ -58,30 +66,54 @@ class SettingsGUI:
         database_label.pack(in_=network_frame, side=RIGHT)
 
         # Apply Email Labels & Input boxes to the EConn_Frame
+        #     Email Server Input Box
         eserver_label = Label(econn_frame, text='Email Server:')
         eserver_txtbox = Entry(econn_frame, textvariable=self.email_server)
         eserver_label.grid(row=0, column=0, padx=8, pady=5, sticky='w')
         eserver_txtbox.grid(row=0, column=1, padx=13, pady=5, sticky='e')
 
+        #     Email Port Input Box
         eport_label = Label(econn_frame, text='Email Port:')
         eport_txtbox = Entry(econn_frame, textvariable=self.email_port)
         eport_label.grid(row=1, column=0, padx=8, pady=5, sticky='w')
         eport_txtbox.grid(row=1, column=1, padx=13, pady=5, sticky='e')
 
+        #     Email User Name Input Box
         euname_label = Label(econn_frame, text='Email User Name:')
         euname_txtbox = Entry(econn_frame, textvariable=self.email_user_name)
         euname_label.grid(row=2, column=0, padx=8, pady=5, sticky='w')
         euname_txtbox.grid(row=2, column=1, padx=13, pady=5, sticky='e')
 
+        #     Email User Pass Input Box
         eupass_label = Label(econn_frame, text='Email User Pass:')
         eupass_txtbox = Entry(econn_frame, textvariable=self.email_user_pass)
         eupass_label.grid(row=3, column=0, padx=8, pady=5, sticky='w')
         eupass_txtbox.grid(row=3, column=1, padx=13, pady=5, sticky='e')
+
+        # Apply Email Labels & Input boxes to the EMsg_Frame
+        #     From Email Address Input Box
+        efrom_label = Label(emsg_frame, text='From Addr:')
+        efrom_txtbox = Entry(emsg_frame, textvariable=self.email_from)
+        efrom_label.grid(row=0, column=0, padx=8, pady=5, sticky='w')
+        efrom_txtbox.grid(row=0, column=1, padx=13, pady=5, sticky='e')
+
+        #     To Email Address Input Box
+        eto_label = Label(emsg_frame, text='To Addr:')
+        eto_txtbox = Entry(emsg_frame, textvariable=self.email_to)
+        eto_label.grid(row=1, column=0, padx=8, pady=5, sticky='w')
+        eto_txtbox.grid(row=1, column=1, padx=13, pady=5, sticky='e')
+
+        #     CC Email Address Input Box
+        ecc_label = Label(emsg_frame, text='CC Addr:')
+        ecc_txtbox = Entry(emsg_frame, textvariable=self.email_cc)
+        ecc_label.grid(row=2, column=0, padx=8, pady=5, sticky='w')
+        ecc_txtbox.grid(row=2, column=1, padx=13, pady=5, sticky='e')
 
         # Show dialog
         self.main.mainloop()
 
 
 if __name__ == '__main__':
+    header_text = 'Welcome to Send to LV Settings!\nSettings haven''t been established.\nPlease fill out all empty fields below:'
     myobj = SettingsGUI()
     myobj.build_gui()
