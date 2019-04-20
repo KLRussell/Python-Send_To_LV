e86822fbacf7af7f4e09234a205316f1a8ed916a
Kevin Russell <kevinr.work@gmail.com>
2019-04-20 01:36:51 -0400

    Added GUI Content

--- Settings.py
+++ Settings.py
@@ -3,6 +3,9 @@ from tkinter import messagebox
 
 
 class SettingsGUI:
+    listbox = None
+    listbox2 = None
+
     def __init__(self, header=None):
         if header:
             self.header_text = header
@@ -19,33 +22,38 @@ class SettingsGUI:
         self.email_from = StringVar()
         self.email_to = StringVar()
         self.email_cc = StringVar()
+        self.sql_table = StringVar()
 
     def build_gui(self):
         # Set GUI Geometry and GUI Title
-        self.main.geometry('509x565+500+100')
+        self.main.geometry('509x595+500+100')
         self.main.title('Send to LV Settings')
         self.main.resizable(False, False)
 
         # Set GUI Frames
         header_frame = Frame(self.main)
-        network_frame = LabelFrame(self.main, text='Network Settings', width=500, height=70)
-        email_frame = LabelFrame(self.main, text='E-mail', width=500, height=170)
-        econn_frame = LabelFrame(email_frame, text='Settings', width=250, height=170)
-        emsg_frame = LabelFrame(email_frame, text='Message', width=250, height=170)
-        stlv_frame = LabelFrame(self.main, text='Send To LV', width=500, height=200)
-        stlv_list_frame = LabelFrame(self.main, text='Table Columns', width=300, height=200)
-        stlv_settings_frame = LabelFrame(self.main, text='Settings', width=200, height=200)
+        network_frame = LabelFrame(self.main, text='Network Settings', width=508, height=70)
+        email_frame = LabelFrame(self.main, text='E-mail', width=508, height=170)
+        econn_frame = LabelFrame(email_frame, text='Settings', width=252, height=170)
+        emsg_frame = LabelFrame(email_frame, text='Message', width=253, height=170)
+        stlv_frame = LabelFrame(self.main, text='Send To LV', width=508, height=200)
+        stlv_list_frame = LabelFrame(stlv_frame, text='SQL Table Columns', width=227, height=200)
+        stlv_list_frame2 = Frame(stlv_frame, width=50, height=200)
+        stlv_list_frame3 = LabelFrame(stlv_frame, text='Table Columns Selected', width=228, height=200)
+        stlv_settings_frame = LabelFrame(stlv_frame, text='SQL Table', width=505, height=70)
         buttons_frame = Frame(self.main)
 
         # Apply Frames into GUI
         header_frame.pack()
         network_frame.pack(fill="both")
         email_frame.pack(fill="both")
-        econn_frame.grid(row=3, column=0, ipady=4)
-        emsg_frame.grid(row=3, column=1, ipady=20)
+        econn_frame.grid(row=0, column=0, ipady=5)
+        emsg_frame.grid(row=0, column=1, ipady=20)
         stlv_frame.pack(fill="both")
-        stlv_list_frame.pack(in_=stlv_frame, fill="both", side=RIGHT)
-        stlv_settings_frame.pack(in_=stlv_frame, fill="both", side=LEFT)
+        stlv_settings_frame.grid(row=0, columnspan=3, ipady=2)
+        stlv_list_frame.grid(row=1, column=0, rowspan=4, ipady=2, sticky='e')
+        stlv_list_frame2.grid(row=1, column=1, rowspan=4, ipady=2)
+        stlv_list_frame3.grid(row=1, column=2, rowspan=4, ipady=2, sticky='w')
         buttons_frame.pack()
 
         # Apply Header text to Header_Frame that describes purpose of GUI
@@ -109,8 +117,46 @@ class SettingsGUI:
         ecc_label.grid(row=2, column=0, padx=8, pady=5, sticky='w')
         ecc_txtbox.grid(row=2, column=1, padx=13, pady=5, sticky='e')
 
+        # Apply Line Verification SQL Table Label & Input boxes to the EMsg_Frame
+        #     From Email Address Input Box
+        lv_sqltbl_txtbox = Entry(stlv_settings_frame, textvariable=self.sql_table, width=76)
+        lv_sqltbl_txtbox.grid(row=0, columnspan=3, padx=20, pady=5)
+
+        lv_scrollbar = Scrollbar(stlv_list_frame, orient="vertical")
+        lv_scrollbar2 = Scrollbar(stlv_list_frame, orient="horizontal")
+        self.listbox = Listbox(stlv_list_frame, selectmode=SINGLE, width=25, yscrollcommand=lv_scrollbar.set,
+                               xscrollcommand=lv_scrollbar2.set)
+        self.listbox.grid(row=0, column=0, rowspan=4, padx=8, pady=5)
+
+        lv_scrollbar3 = Scrollbar(stlv_list_frame3, orient="vertical")
+        lv_scrollbar4 = Scrollbar(stlv_list_frame3, orient="horizontal")
+        self.listbox2 = Listbox(stlv_list_frame3, selectmode=SINGLE, width=25, yscrollcommand=lv_scrollbar3.set,
+                                xscrollcommand=lv_scrollbar4.set)
+        self.listbox2.grid(row=0, column=0, rowspan=4, padx=8, pady=5)
+
+        move_all_button = Button(stlv_list_frame2, text='>>', width=4, command=self.move_right_all)
+        move_all_button.grid(row=0, column=1, pady=10)
+        move_all_button = Button(stlv_list_frame2, text='>', width=4, command=self.move_right)
+        move_all_button.grid(row=1, column=1, pady=5)
+        move_all_button = Button(stlv_list_frame2, text='<', width=4, command=self.move_left)
+        move_all_button.grid(row=2, column=1, pady=5)
+        move_all_button = Button(stlv_list_frame2, text='<<', width=4, command=self.move_left_all)
+        move_all_button.grid(row=3, column=1, pady=5)
+
         # Show dialog
         self.main.mainloop()
+
+    def move_right_all(self):
+        print('all right')
+
+    def move_right(self):
+        print('right')
+
+    def move_left_all(self):
+        print('all left')
+
+    def move_left(self):
+        print('left')
 
 
 if __name__ == '__main__':
